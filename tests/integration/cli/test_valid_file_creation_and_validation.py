390e1785090ef9fe5f7a234f23da1fbac09271dc
Zebedee Nicholls <zebedee.nicholls@climate-resource.com>
2024-07-23 15:54:56 +0200

    Add testing module

--- tests/integration/cli/test_valid_file_creation_and_validation.py
+++ tests/integration/cli/test_valid_file_creation_and_validation.py
@@ -9,7 +9,6 @@ import os
 from pathlib import Path
 from unittest.mock import patch
 
-import cftime
 import numpy as np
 import pint
 import pint_xarray  # noqa: F401 # required to activate pint accessor
@@ -20,9 +19,9 @@ from input4mips_validation.cli import app
 from input4mips_validation.database import Input4MIPsDatabaseEntryFile
 from input4mips_validation.dataset import (
     Input4MIPsDataset,
-    Input4MIPsDatasetMetadataDataProducerMinimum,
     Input4MIPsDatasetMetadataDataProducerMultipleVariableMinimum,
 )
+from input4mips_validation.testing import get_valid_ds_min_metadata_example
 from input4mips_validation.validation import validate_file
 
 UR = pint.get_application_registry()
@@ -33,50 +32,15 @@ runner = CliRunner()
 DEFAULT_TEST_INPUT4MIPS_CV_SOURCE = str(
     (Path(__file__).parent / ".." / ".." / "test-data" / "cvs" / "default").absolute()
 )
-# Tests to write:
-# - quasi-unit test of passing to function and handling of error raising
-# - help messages
-# - logging
 
 
 def test_validate_written_single_variable_file(tmp_path):
     """
     Test that we can write a single variable file that passes our validate-file CLI
     """
-    metadata_minimum = Input4MIPsDatasetMetadataDataProducerMinimum(
-        grid_label="gn",
-        nominal_resolution="10000 km",
-        product="derived",
-        region="global",
-        source_id="CR-CMIP-0-2-0",
-        target_mip="CMIP",
-    )
+    variable_name = "mole_fraction_of_carbon_dioxide_in_air"
+    ds, metadata_minimum = get_valid_ds_min_metadata_example(variable_id=variable_name)
 
-    lon = np.arange(-165.0, 180.0, 30.0, dtype=np.float64)
-    lat = np.arange(-82.5, 90.0, 15.0, dtype=np.float64)
-    time = [
-        cftime.datetime(y, m, 1) for y in range(2000, 2010 + 1) for m in range(1, 13)
-    ]
-
-    rng = np.random.default_rng()
-    ds_data = UR.Quantity(
-        rng.random((lon.size, lat.size, len(time))),
-        "ppm",
-    )
-
-    ds = xr.Dataset(
-        data_vars={
-            "mole_fraction_of_carbon_dioxide_in_air": (["lat", "lon", "time"], ds_data),
-        },
-        coords=dict(
-            lon=("lon", lon),
-            lat=("lat", lat),
-            time=time,
-        ),
-        attrs={},
-    )
-    # This is a good trick to remember for, e.g. reducing file sizes.
-    ds["lat"].encoding = {"dtype": np.dtypes.Float32DType}
     ds["time"].encoding = {
         "calendar": "proleptic_gregorian",
         "units": "days since 1850-01-01 00:00:00",
@@ -92,11 +56,8 @@ def test_validate_written_single_variable_file(tmp_path):
         input4mips_ds = Input4MIPsDataset.from_data_producer_minimum_information(
             data=ds,
             metadata_minimum=metadata_minimum,
-            dimensions=("time", "lat", "lon"),
             standard_and_or_long_names={
-                "mole_fraction_of_carbon_dioxide_in_air": {
-                    "standard_name": "mole_fraction_of_carbon_dioxide_in_air"
-                }
+                variable_name: {"standard_name": variable_name}
             },
         )
 
@@ -172,6 +133,22 @@ def test_validate_written_multi_variable_file(tmp_path):
     """
     Test that we can write a multi-variable file that passes our validate-file CLI
     """
+    co2_dat, _ = get_valid_ds_min_metadata_example(
+        variable_id="mole_fraction_of_carbon_dioxide_in_air"
+    )
+    ch4_dat, _ = get_valid_ds_min_metadata_example(
+        variable_id="mole_fraction_of_methane_in_air", units="ppb"
+    )
+
+    ds = xr.merge([co2_dat, ch4_dat])
+    ds["time"].encoding = {
+        "calendar": "proleptic_gregorian",
+        "units": "days since 1850-01-01 00:00:00",
+        # Time has to be encoded as float
+        # to ensure that half-days etc. are handled.
+        "dtype": np.dtypes.Float32DType,
+    }
+
     metadata_minimum = Input4MIPsDatasetMetadataDataProducerMultipleVariableMinimum(
         dataset_category="GHGConcentrations",
         grid_label="gn",
@@ -182,43 +159,6 @@ def test_validate_written_multi_variable_file(tmp_path):
         source_id="CR-CMIP-0-2-0",
         target_mip="CMIP",
     )
-
-    lon = np.arange(-165.0, 180.0, 30.0, dtype=np.float64)
-    lat = np.arange(-82.5, 90.0, 15.0, dtype=np.float64)
-    time = [
-        cftime.datetime(y, m, 1) for y in range(2000, 2010 + 1) for m in range(1, 13)
-    ]
-
-    rng = np.random.default_rng()
-    ds_data = UR.Quantity(
-        rng.random((lon.size, lat.size, len(time))),
-        "ppm",
-    )
-
-    ds = xr.Dataset(
-        data_vars={
-            "mole_fraction_of_carbon_dioxide_in_air": (["lat", "lon", "time"], ds_data),
-            "mole_fraction_of_methane_in_air": (
-                ["lat", "lon", "time"],
-                ds_data * 2.3 * UR.Quantity(1, "ppb / ppm"),
-            ),
-        },
-        coords=dict(
-            lon=("lon", lon),
-            lat=("lat", lat),
-            time=time,
-        ),
-        attrs={},
-    )
-    # This is a good trick to remember for, e.g. reducing file sizes.
-    ds["lat"].encoding = {"dtype": np.dtypes.Float32DType}
-    ds["time"].encoding = {
-        "calendar": "proleptic_gregorian",
-        "units": "days since 1850-01-01 00:00:00",
-        # Time has to be encoded as float
-        # to ensure that half-days etc. are handled.
-        "dtype": np.dtypes.Float32DType,
-    }
 
     with patch.dict(
         os.environ,

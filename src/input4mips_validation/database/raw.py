7b7580fd961137b4aa3e46ee011f044b5577110a
Zebedee Nicholls <zebedee.nicholls@climate-resource.com>
2024-08-01 12:10:59 +0200

    Change default validated status to None

--- src/input4mips_validation/database/raw.py
+++ src/input4mips_validation/database/raw.py
@@ -187,8 +187,14 @@ class Input4MIPsDatabaseEntryFileRaw:
     This is scraped from the ESGF.
     """
 
-    validated_input4mips: bool = False
-    """Has this file been validated by the input4MIPs team?"""
+    validated_input4mips: Union[bool, None] = None
+    """
+    Has this file been validated by the input4MIPs team?
+
+    A value of `None` indicates that the file has not been validated.
+    A value of `True` indicates that the file has been validated and passed validation.
+    A value of `False` indicates that the file has been validated and failed validation.
+    """
 
     xlink: Union[list[str], None] = None
     """Cross-link to more information about the file (DOI?)"""

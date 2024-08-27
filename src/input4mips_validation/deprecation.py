4ad376e21f712b4a88c262487a8a90cd48848ad7
Zebedee Nicholls <zebedee.nicholls@climate-resource.com>
2024-08-27 08:46:21 +0200

    First CHANGELOG

--- src/input4mips_validation/deprecation.py
+++ src/input4mips_validation/deprecation.py
@@ -7,7 +7,9 @@ from __future__ import annotations
 import warnings
 
 
-def raise_deprecation_warning(name: str, removed_in: str, stacklevel: int = 3) -> None:
+def raise_deprecation_warning(
+    name: str, removed_in: str, use_instead: str, stacklevel: int = 3
+) -> None:
     """
     Raise a deprecation warning
 
@@ -19,8 +21,13 @@ def raise_deprecation_warning(name: str, removed_in: str, stacklevel: int = 3) -
     removed_in
         Version in which the callable will be removed
 
+    use_instead
+        Instructions on what to use instead of the deprecated callable
+
     stacklevel
         Stack level to show with the warning
     """
-    message = f"{name} will be removed in {removed_in}"
+    message = (
+        f"{name} will be removed in {removed_in}. Instead, please use {use_instead}."
+    )
     warnings.warn(message, DeprecationWarning, stacklevel=stacklevel)

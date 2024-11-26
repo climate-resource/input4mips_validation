ea94680cf9caa71ac4f914e768bea350a0481a8f
Zebedee Nicholls <zebedee.nicholls@climate-resource.com>
2024-11-26 20:23:20 +0100

    Try updating RtD workflow

---
+++ scripts/download-cf-standard-name-table.py
@@ -0,0 +1,18 @@
+"""
+Grab the CF standard name table
+
+Workaround for https://github.com/readthedocs/readthedocs.org/issues/11763
+while we wait for
+"""
+
+import pooch
+
+DOWNLOADER = pooch.HTTPDownloader(
+    # https://github.com/readthedocs/readthedocs.org/issues/11763
+    headers={"User-Agent": "input4mips-validation"}
+)
+pooch.retrieve(
+    "https://raw.githubusercontent.com/cf-convention/cf-convention.github.io/master/Data/cf-standard-names/current/src/cf-standard-name-table.xml",
+    known_hash=None,
+    downloader=DOWNLOADER,
+)

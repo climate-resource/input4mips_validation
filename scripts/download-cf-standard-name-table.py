fa13af80540ffc8b34263185db316552d6244beb
Zebedee Nicholls <zebedee.nicholls@climate-resource.com>
2024-11-26 20:35:34 +0100

    CHANGELOG

--- scripts/download-cf-standard-name-table.py
+++ scripts/download-cf-standard-name-table.py
@@ -2,7 +2,7 @@
 Grab the CF standard name table
 
 Workaround for https://github.com/readthedocs/readthedocs.org/issues/11763
-while we wait for
+while we wait for https://github.com/xarray-contrib/cf-xarray/pull/547.
 """
 
 import pooch

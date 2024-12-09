baf73d910dc081dde7d65e68a5d49aba06ee657a
Zebedee Nicholls <zebedee.nicholls@climate-resource.com>
2024-12-09 14:30:14 +0100

    Deprecate functions

--- src/input4mips_validation/validation/datasets_to_write_to_disk.py
+++ src/input4mips_validation/validation/datasets_to_write_to_disk.py
@@ -12,12 +12,10 @@ from typing import Callable, Union
 import xarray as xr
 
 from input4mips_validation.cvs import Input4MIPsCVs
-from input4mips_validation.deprecation import raise_deprecation_warning
 from input4mips_validation.validation.creation_date import validate_creation_date
 from input4mips_validation.validation.error_catching import (
     MissingAttributeError,
     ValidationResultsStore,
-    get_catch_error_decorator,
 )
 from input4mips_validation.validation.tracking_id import validate_tracking_id
 from input4mips_validation.validation.variable_id import validate_variable_id
@@ -158,58 +156,3 @@ def get_ds_to_write_to_disk_validation_result(
         )(ds, attribute, validation_function)
 
     return vrs
-
-
-def validate_ds_to_write_to_disk(
-    ds: xr.Dataset, out_path: Path, cvs: Input4MIPsCVs
-) -> None:
-    """
-    Validate a dataset that is going to be written to disk
-
-    /// danger | Deprecated
-    `validate_ds_to_write_to_disk` was deprecated in v0.12.0.
-    It will be removed in v0.14.0.
-    Please use [`get_ds_to_write_to_disk_validation_result`][input4mips_validation.validation.datasets_to_write_to_disk.get_ds_to_write_to_disk_validation_result]
-    instead because it provides greater control.
-    ///
-
-    Parameters
-    ----------
-    ds
-        Dataset to write to disk.
-        May contain one or more variables.
-
-    out_path
-        Path in which to the dataset will be written
-
-    cvs
-        CVs to use to validate the dataset before writing
-
-    Raises
-    ------
-    InvalidDatasetToWriteToDiskError
-        Given the values of `out_path` and `cvs`,
-        `ds` is not valid for writing to disk.
-    """  # noqa: E501
-    raise_deprecation_warning(
-        "validate_ds_to_write_to_disk",
-        removed_in="0.14.0",
-        use_instead=(
-            "`get_ds_to_write_to_disk_validation_result`, "
-            "then process the result to suit your use case"
-        ),
-    )
-
-    caught_errors: list[tuple[str, Exception]] = []
-    checks_performed: list[str] = []
-    catch_error = get_catch_error_decorator(caught_errors, checks_performed)
-
-    catch_error(
-        validate_creation_date, call_purpose="Validate the creation_date attribute"
-    )(ds.attrs["creation_date"])
-
-    if caught_errors:
-        raise InvalidDatasetToWriteToDiskError(
-            ds=ds,
-            error_container=caught_errors,
-        )
--- src/input4mips_validation/validation/error_catching.py
+++ src/input4mips_validation/validation/error_catching.py
@@ -13,7 +13,6 @@ from attrs import define, field
 from loguru import logger
 from typing_extensions import ParamSpec
 
-from input4mips_validation.deprecation import raise_deprecation_warning
 from input4mips_validation.logging import (
     LOG_LEVEL_INFO_INDIVIDUAL_CHECK,
     LOG_LEVEL_INFO_INDIVIDUAL_CHECK_ERROR,
@@ -280,92 +279,6 @@ class CatchErrorDecoratorLike(Protocol):
         """
         Get wrapped version of a function
         """
-
-
-def get_catch_error_decorator(
-    error_container: list[tuple[str, Exception]],
-    checks_performed_container: list[str],
-) -> CatchErrorDecoratorLike:
-    """
-    Get a decorator which can be used to collect errors without stopping the program
-
-    /// danger | Deprecated
-    `get_catch_error_decorator` was deprecated in v0.12.0.
-    It will be removed in v0.14.0.
-    Please use [`ValidationResultsStore`][input4mips_validation.validation.error_catching.ValidationResultsStore]
-    instead because it provides greater control.
-    ///
-
-    Parameters
-    ----------
-    error_container
-        The list in which to store the things being run and the caught errors
-
-    checks_performed_container
-        List which stores the checks that were performed
-
-    Returns
-    -------
-    :
-        Decorator which can be used to collect errors
-        that occur while calling callables.
-    """  # noqa: E501
-    raise_deprecation_warning(
-        "get_catch_error_decorator",
-        removed_in="0.14.0",
-        use_instead="`ValidationResultStore`",
-    )
-
-    def catch_error_decorator(
-        func_to_call: Callable[P, T], call_purpose: str
-    ) -> Callable[P, T | None]:
-        """
-        Decorate a callable such that any raised errors are caught
-
-        This allows the program to keep running even if errors occur.
-
-        If the function raises no error,
-        a confirmation that it ran successfully is logged.
-
-        Parameters
-        ----------
-        func_to_call
-            Function to call
-
-        call_purpose
-            A description of the purpose of the call.
-            This helps us create clearer error messages for the steps which failed.
-
-        Returns
-        -------
-        :
-            Decorated function
-        """
-
-        @wraps(func_to_call)
-        def decorated(*args: P.args, **kwargs: P.kwargs) -> T | None:
-            try:
-                checks_performed_container.append(call_purpose)
-                res = func_to_call(*args, **kwargs)
-
-            except Exception as exc:
-                logger.log(
-                    LOG_LEVEL_INFO_INDIVIDUAL_CHECK_ERROR.name,
-                    f"{call_purpose} raised an error ({type(exc).__name__})",
-                )
-                error_container.append((call_purpose, exc))
-                return None
-
-            logger.log(
-                LOG_LEVEL_INFO_INDIVIDUAL_CHECK.name,
-                f"{call_purpose} ran without error",
-            )
-
-            return res
-
-        return decorated
-
-    return catch_error_decorator
 
 
 class MissingAttributeError(KeyError):

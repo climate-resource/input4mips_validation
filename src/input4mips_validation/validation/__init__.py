1235d10426f971efdbe9790a8aaeaccb2e7e1e46
Zebedee Nicholls <zebedee.nicholls@climate-resource.com>
2024-12-09 14:32:50 +0100

    Fix up imports

--- src/input4mips_validation/validation/__init__.py
+++ src/input4mips_validation/validation/__init__.py
@@ -8,7 +8,5 @@ from input4mips_validation.validation.exceptions import (
     InvalidFileError,
     InvalidTreeError,
 )
-from input4mips_validation.validation.file import validate_file
-from input4mips_validation.validation.tree import validate_tree
 
-__all__ = ["InvalidFileError", "InvalidTreeError", "validate_file", "validate_tree"]
+__all__ = ["InvalidFileError", "InvalidTreeError"]

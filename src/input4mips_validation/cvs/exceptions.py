246139382e05d921ff05b466e3de515a03db7245
Zebedee Nicholls <zebedee.nicholls@climate-resource.com>
2024-12-15 17:37:51 +0100

    Pass tests

--- src/input4mips_validation/cvs/exceptions.py
+++ src/input4mips_validation/cvs/exceptions.py
@@ -47,3 +47,48 @@ class ValueNotAllowedByCVsError(ValueError):
         )
 
         super().__init__(error_msg)
+
+
+class ValueInconsistentWithCVsError(ValueError):
+    """
+    Raised when a value is inconsistent with the value expected by the CVs
+    """
+
+    def __init__(  # noqa: PLR0913
+        self,
+        value: str,
+        expected_value: str,
+        cv_component: str,
+        cv_component_dependent_on: str,
+        cv_entry_dependenty_component: Any,
+    ) -> None:
+        """
+        Initialise the error
+
+        Parameters
+        ----------
+        value
+            The invalid value
+
+        expected_value
+            The value expected by the CVs
+
+        cv_component
+            The component of the CVs being considered
+
+        cv_component_dependent_on
+            The component of the CVs that the component being considered is dependent on
+
+        cv_entry_dependenty_component
+            The entry that defines the value, as it is in the CVs
+        """
+        error_msg = (
+            f"The value provided for {cv_component} was {value!r}. "
+            "According to the CVs, "
+            f"{cv_component} depends on the value of {cv_component_dependent_on}. "
+            f"As a result, {cv_component} must be {expected_value!r}. "
+            f"If helpful, the full CV entry for {cv_component_dependent_on} is: "
+            f"{cv_entry_dependenty_component}."
+        )
+
+        super().__init__(error_msg)

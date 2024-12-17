ce11ee2bb942baa79278da6417e6cf74049dd5e7
Zebedee Nicholls <zebedee.nicholls@climate-resource.com>
2024-12-17 19:22:19 +0100

    Add failing tests

---
+++ tests/unit/validation/test_external_variables.py
@@ -0,0 +1,80 @@
+"""
+Tests of our external variables validation
+"""
+
+from __future__ import annotations
+
+import re
+from contextlib import nullcontext as does_not_raise
+
+import pytest
+
+from input4mips_validation.validation.external_variables import (
+    validate_external_variables,
+)
+
+EXP_ERROR_MSG = "".join(
+    [
+        re.escape(
+            "The `external_variables` attribute contains invalid characters. "
+            "Only alphanumeric characters and underscores are allowed. "
+            "The list of variables should be whitespace-separated."
+        ),
+        r"Received external_variables='.*'\.",
+    ]
+)
+
+
+@pytest.mark.parametrize(
+    "external_variables, expectation",
+    (
+        pytest.param(
+            "co2",
+            does_not_raise(),
+            id="valid_value",
+        ),
+        pytest.param(
+            "co2_mole",
+            does_not_raise(),
+            id="valid_value_underscore",
+        ),
+        pytest.param(
+            "co2 ch4",
+            does_not_raise(),
+            id="valid_value_whitespace_separated",
+        ),
+        pytest.param(
+            "co2 ch4_mole",
+            does_not_raise(),
+            id="valid_value_whitespace_separated_underscore_1",
+        ),
+        pytest.param(
+            "co2_mole ch4",
+            does_not_raise(),
+            id="valid_value_whitespace_separated_underscore_2",
+        ),
+        pytest.param(
+            "co2_mole ch4_mole",
+            does_not_raise(),
+            id="valid_value_whitespace_separated_underscore_3",
+        ),
+        pytest.param(
+            "co2,ch4",
+            pytest.raises(ValueError, match=EXP_ERROR_MSG),
+            id="invalid_value_comma_separated",
+        ),
+        pytest.param(
+            "co2;ch4",
+            pytest.raises(ValueError, match=EXP_ERROR_MSG),
+            id="invalid_value_semicolon_separated",
+        ),
+        pytest.param(
+            "co2-mole",
+            pytest.raises(ValueError, match=EXP_ERROR_MSG),
+            id="invalid_value_hyphen",
+        ),
+    ),
+)
+def test_external_variables_validation(external_variables, expectation):
+    with expectation:
+        validate_external_variables(external_variables)

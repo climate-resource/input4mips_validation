a838f9cf1333dff490e34f93b46b15b8c65aeb20
Zebedee Nicholls <zebedee.nicholls@climate-resource.com>
2024-12-23 09:26:55 +0100

    No logging in db create

--- src/input4mips_validation/database/creation.py
+++ src/input4mips_validation/database/creation.py
@@ -6,58 +6,15 @@ from __future__ import annotations
 
 from collections.abc import Iterable
 from pathlib import Path
-from typing import Any
 
-import input4mips_validation.logging_config
 from input4mips_validation.cvs.loading import load_cvs
 from input4mips_validation.database.database import Input4MIPsDatabaseEntryFile
 from input4mips_validation.inference.from_data import FrequencyMetadataKeys
-from input4mips_validation.logging import setup_logging
-from input4mips_validation.logging_config import (
-    LoggingConfigSerialisedType,
-    deserialise_logging_config,
-    serialise_logging_config,
-)
 from input4mips_validation.parallelisation import run_parallel
 from input4mips_validation.xarray_helpers.variables import (
     XRVariableHelper,
     XRVariableProcessorLike,
 )
-
-
-def create_db_file_entry_with_logging(
-    file: Path,
-    /,
-    logging_config_serialised: LoggingConfigSerialisedType,
-    **kwargs: Any,
-) -> Input4MIPsDatabaseEntryFile:
-    """
-    Create database file entries, with the logging setup matching main
-
-    Parameters
-    ----------
-    file
-        File for which to create the entry
-
-    logging_config_serialised
-        Logging configuration to use (serialised version thereof)
-
-    kwargs
-        Passed to
-        [`Input4MIPsDatabaseEntryFile.from_file`][input4mips_validation.database.database.Input4MIPsDatabaseEntryFile.from_file]
-
-    Returns
-    -------
-    :
-        Created database entry for `file`
-    """
-    logging_config = deserialise_logging_config(logging_config_serialised)
-    setup_logging(
-        enable=logging_config is not None,
-        logging_config=logging_config,
-    )
-
-    return Input4MIPsDatabaseEntryFile.from_file(file, **kwargs)
 
 
 def create_db_file_entries(  # noqa: PLR0913
@@ -102,18 +59,11 @@ def create_db_file_entries(  # noqa: PLR0913
     """
     cvs = load_cvs(cv_source=cv_source)
 
-    # Might be fixable if we use multiprocessing more directly,
-    # see e.g. https://github.com/znichollscr/loguru/blob/61963240dece1979961ec43840e06a1f396479c4/tests/test_multiprocessing.py#L243
-    logging_config_serialised = serialise_logging_config(
-        input4mips_validation.logging_config.LOGGING_CONFIG
-    )
-
     db_entries = run_parallel(
-        func_to_call=create_db_file_entry_with_logging,
+        func_to_call=Input4MIPsDatabaseEntryFile.from_file,
         iterable_input=files,
         input_desc="files",
         n_processes=n_processes,
-        logging_config_serialised=logging_config_serialised,
         cvs=cvs,
         xr_variable_processor=xr_variable_processor,
         frequency_metadata_keys=frequency_metadata_keys,
